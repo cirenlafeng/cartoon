9d9e2e618c4fcbf6c14dec281fe35393adedd357
chekai <786401300@qq.com>
2017-12-13 11:25:07 +0800

    update

--- conf/db.php
+++ conf/db.php
@@ -19,8 +19,8 @@ $sysConfig['api']['url'] = 'http://comicstest.mobibookapp.com/api/cartoon/set_te
 $sysConfig['api']['key'] = '47b84030d53d2f94339e43c552062ff9';
 
 //正式服
-// $sysConfig['api']['url'] = 'http://admin.mobibookapp.com/api/cartoon/set_temp_xsda486_4asdfg_5de_8r7w8s_df45s';
-// $sysConfig['api']['key'] = 'd5aafc3f489da27f4582d7a2ad76764069247_99999A';
+// $sysConfig['api']['url'] = 'http://comics.mobibookapp.com/api/cartoon/set_temp_xsda486_4asdfg_5de_8r7w8s_df45s';
+// $sysConfig['api']['key'] = '47b84030d53d2f94339e43c552062ff9';
 
 //图片cdn
 $sysConfig['RRC_GATHER_URL'] = 'http://pushcdn.mysada.com/api.php?key=58307deb71dd486ef8afc742056780c0';
